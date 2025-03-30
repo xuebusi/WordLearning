8b747cbe3a816fd7fc7fb1a3db01ca11bacd8ffb
swiftui <xuebusi@qq.com>
2025-03-30 13:18:48 +0800

    init

---
+++ WordLearning/WordLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordLearningApp.swift
+//  WordLearning
+//
+//  Created by shiyanjun on 2025/3/30.
+//
+
+import SwiftUI
+
+@main
+struct WordLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
